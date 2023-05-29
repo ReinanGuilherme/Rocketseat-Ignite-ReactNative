7162f6a0f0b1b23ff66f22be0c7ab92544a46845
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.local>
2023-05-29 14:49:52 -0300

    projeto 02 - incompleto

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '@components/Header';
+import { Container } from './styles';
+import React from 'react';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <Header showBackButton/>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
