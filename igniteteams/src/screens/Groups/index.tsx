670d5a05ede7f709daf11f7fba6a48c326c01d06
ReinanGuilherme <r.guilhermedeoliveira3dev@gmail.com>
2023-05-29 21:21:26 -0300

    projeto 02 incompleto

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,11 +1,33 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
-import React from 'react';
+import React, { useState } from 'react';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState([])
+
   return (
     <Container>
-      <Header showBackButton/>
+      <Header/>
+
+      <Highlight
+      title='Turmas'
+      subtitle='Jogue com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => <GroupCard title={item}/>}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={<ListEmpty message='Que tal cadastrar a primeira turma?'/>}
+      />
+
+      
     </Container>
   );
 }
