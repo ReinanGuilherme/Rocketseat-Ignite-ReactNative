c86a9e84b127a533ed2e2d3dff92ec2507c46690
ReinanGuilherme <r.guilhermedeoliveira3dev@gmail.com>
2023-09-03 18:31:22 -0300

    atualizando

---
+++ ignitegym/src/components/UserPhoto.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Image, IImageProps } from 'native-base';
+
+type Props = IImageProps & {
+    size: number
+}
+
+export function UserPhoto({size, ...rest}: Props) {
+  return (
+    <Image 
+        w={size}
+        h={size}
+        rounded="full"
+        borderColor="gray.400"
+        {...rest}    
+    />
+  );
+}
--- ignitegym/src/screens/Home.tsx
+++ ignitegym/src/screens/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Center, Text } from 'native-base';
+import { VStack } from 'native-base';
+import { HomeHeader } from '../components/HomeHeader';
+import { Group } from '../components/Group';
 
 export function Home() {
   return (
-    <Center flex={1}>
-        <Text>
-            Home
-        </Text>
-    </Center>
+    <VStack flex={1}>
+        <HomeHeader />
+
+        <Group name='COSTAS' />
+    </VStack>
   );
 }
