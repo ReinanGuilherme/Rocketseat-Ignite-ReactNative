ca77d86c8717521a1646bbdf3fb63dfeb7bae03c
ReinanGuilherme <r.guilhermedeoliveira3dev@gmail.com>
2023-09-03 20:31:55 -0300

    atualizando

--- ignitegym/src/components/Group.tsx
+++ ignitegym/src/components/Group.tsx
@@ -3,9 +3,10 @@ import { Text, Pressable, IPressableProps } from 'native-base';
 
 type Props = IPressableProps & {
     name: string
+    isActived: boolean
 }
 
-export function Group({name, ...rest}: Props) {
+export function Group({name,isActived, ...rest}: Props) {
   return (
     <Pressable
         mr={3}
@@ -16,9 +17,14 @@ export function Group({name, ...rest}: Props) {
         justifyContent="center"
         alignItems="center"
         overflow="hidden"
+        isPressed={isActived}
+        _pressed={{
+            borderColor: "green.500",
+            borderWidth: 1
+        }}
     {...rest}>
         <Text
-        color="gray.200"
+        color={isActived ? "green.200" : "green.500"}
         textTransform="uppercase"
         fontSize="xs"
         fontWeight="bold"
--- ignitegym/src/screens/Home.tsx
+++ ignitegym/src/screens/Home.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
-import { VStack } from 'native-base';
+import React, { useState } from 'react';
+import { HStack, VStack, FlatList, Heading, Text } from 'native-base';
 import { HomeHeader } from '../components/HomeHeader';
 import { Group } from '../components/Group';
+import { ExerciseCard } from '../components/ExerciseCard';
 
 export function Home() {
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro']);
+    const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
+    const [groupSelected, setGroupSelected] = useState('Costas');
+
   return (
     <VStack flex={1}>
         <HomeHeader />
 
-        <Group name='COSTAS' />
+        <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+        <Group 
+            name={item}
+            isActived={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+            />
+        )}
+        horizontal
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{px: 8}}
+        my={10}
+        maxH={10}
+        
+        />
+
+        <VStack flex={1} px={8}>
+            <HStack justifyContent="space-between" mb={5}>
+                <Heading color="gray.200" fontSize="md">
+                    Exercicios
+                </Heading>
+
+                <Text color="gray.200" fontSize="sm">
+                    {exercises.length}
+                </Text>
+            </HStack>
+
+            <FlatList 
+                data={exercises}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <ExerciseCard />
+                )}
+                showsVerticalScrollIndicator={false}
+                _contentContainerStyle={{
+                    paddingBottom: 20
+                }}
+            />
+        </VStack>
     </VStack>
   );
 }
