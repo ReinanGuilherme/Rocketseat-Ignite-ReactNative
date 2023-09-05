476f4b2902f24e1cdaea896df7ccbdc24796db6a
ReinanGuilherme <r.guilhermedeoliveira3dev@gmail.com>
2023-09-04 21:14:25 -0300

    atualizando

--- ignitegym/src/screens/History.tsx
+++ ignitegym/src/screens/History.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { Heading, SectionList, Text, VStack } from 'native-base';
+import { ScreenHeader } from '../components/ScreenHeader';
+import { HistoryCard } from '../components/HistoryCard';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ["Puxada frontal", "Remada unilateral"]
+    },
+    {
+      title: '27.08.22',
+      data: ["Puxada frontal"]
+    }
+  ]);
+
   return (
-    <Center flex={1}>
-        <Text>
-            Home
-        </Text>
-    </Center>
+    <VStack flex={1}>
+        <ScreenHeader title='Histórico' />
+
+        <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercicios registrados ainda.
+          </Text>
+        )}
+      />
+    </VStack>
   );
 }
