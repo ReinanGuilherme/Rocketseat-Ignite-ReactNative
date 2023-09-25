c6694458dfcdb52f89cc21d3b54f8c57e905c4e8
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2023-09-25 17:19:17 -0300

    finalizando as interfaces

--- ignitegym/src/screens/Exercise.tsx
+++ ignitegym/src/screens/Exercise.tsx
@@ -1,11 +1,15 @@
 import { TouchableOpacity } from 'react-native';
-import { HStack, Heading, Icon, Text, VStack } from 'native-base';
+import { Box, HStack, Heading, Icon, Image, ScrollView, Text, VStack } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { AppNavigatorRoutesProps } from '../routes/app.routes';
+import { Button } from '../components/Button';
+import React from 'react';
 
 import BodySvg from '../assets/body.svg';
-import React from 'react';
+import SeriesSvg from '../assets/series.svg';
+import RepetitionsSvg from '../assets/repetitions.svg';
+
 
 export function Exercise() {
 
@@ -16,6 +20,7 @@ export function Exercise() {
   }
 
   return (
+    
     <VStack flex={1}>
       <VStack px={8} bg="gray.600" pt={12}>
         <TouchableOpacity onPress={handleGoBack}>
@@ -41,6 +46,44 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
+
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            w="full"
+            h={80}
+            source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+            alt="Nome do exercício"
+            mb={3}
+            resizeMode="cover"
+            rounded="lg"
+          />
+
+          <Box bg="gray.600" rounded="md" pb={4} px={4}>
+            <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+              <HStack>
+                <SeriesSvg />
+
+                <Text color="gray.200" ml="2">
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack>
+                <RepetitionsSvg />
+                
+                <Text color="gray.200" ml="2">
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button 
+              title="Marcar como realizado"
+            />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }
