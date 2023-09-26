33b21029ef63759ff77ac3a21605c76d5707625f
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2023-09-26 13:55:57 -0300

    finalizando Galeria de Fotos

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -1,13 +1,54 @@
 import React, { useState } from 'react';
-import { Center,  Heading,  ScrollView, Skeleton, Text, VStack,useTheme  } from 'native-base';
+import { Center,  Heading,  ScrollView, Skeleton, Text, VStack,useTheme, useToast  } from 'native-base';
 import { ScreenHeader } from '../components/ScreenHeader';
 import { UserPhoto } from '../components/UserPhoto';
 import { TouchableOpacity } from 'react-native';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system';
+import { Alert } from 'react-native';
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {    
+    setPhotoIsLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4,4],
+        allowsEditing: true,
+      })
+  
+      if(photoSelected.canceled) {
+        return
+      }
+
+      if(photoSelected.assets[0].uri) {   
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+        
+        if (photoInfo.exists && (photoInfo.size / 1024 / 1024 > 5)) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+  
+    } catch (error) {
+      console.log(error );      
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil'/>
@@ -17,10 +58,10 @@ export function Profile() {
                   photoIsLoading ?
                   <Skeleton w={33} h={33} rounded="full" startColor="gray.500" endColor="gray.400"/>
                   :
-                  <UserPhoto source={{uri: 'https://github.com/reinanguilherme.png'}} alt='Foto do usuário' size={33}/> 
+                  <UserPhoto source={{uri: userPhoto}} alt='Foto do usuário' size={33}/> 
                 }
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleUserPhotoSelect}>
                   <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
                     Alterar foto
                   </Text>
