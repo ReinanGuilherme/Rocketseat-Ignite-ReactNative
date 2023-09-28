c79b5a047248244482af88ce44d7fec21365362f
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2023-09-28 11:59:29 -0300

    finalizando http client

--- ignitegym/src/screens/SignUp.tsx
+++ ignitegym/src/screens/SignUp.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
+import { Alert } from 'react-native';
 
 import LogoSvg from '../assets/logo.svg';
 import BackgroundImg from '../assets/background.png';
@@ -10,6 +11,8 @@ import { Button } from "../components/Button";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { api } from "../services/api";
+import axios from "axios";
+import { AppError } from "../utils/AppError";
 
 type FormDataProps = {
     name: string;
@@ -26,6 +29,8 @@ type FormDataProps = {
 });
 
 export function SignUp() {
+
+    const toast = useToast()
   
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver<FormDataProps>(signUpSchema),
@@ -38,8 +43,20 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    const response = await api.post('/users', { name, email, password });
-    const data = await response.data.json()
+    try {
+        const response = await api.post('/users', { name, email, password });
+        console.log(response.data);
+      } catch (error) {
+        const isAppError = error instanceof AppError
+
+        const title = isAppError ? error.message : "Não foi possivel criar a conta."
+
+        toast.show({
+            title,
+            placement: 'top',
+            bgColor: 'red.500'
+        })
+      }
   }
 
   return (
--- ignitegym/src/services/api.ts
+++ ignitegym/src/services/api.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
+import { AppError } from "../utils/AppError";
 
-export const api = axios.create({
-  baseURL: 'http://192.168.1.11:3333'
+const api = axios.create({
+  baseURL: 'http://192.168.2.161:3333',
+  timeout: 5000
 })
+
+// api.interceptors.request.use((config) => {
+//   return config
+// }, (error) => {
+//   return Promise.reject(error)
+// })
+
+api.interceptors.response.use((response) => response,  error => {
+  if(error.response && error.response.data) {
+    return Promise.reject(new AppError(error.response.data.message))
+  } else {
+    return Promise.reject(error)
+  }
+})
+
+export {api}
